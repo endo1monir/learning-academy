85d88d1f1c91a0b11c6cb9b89f0a1b55387cb427
endo1monir <andrew.monir@yahoo.com>
2022-03-23 06:55:40 +0200

    index

--- learning-academy/app/Http/Controllers/Front/HomepageController.php
+++ learning-academy/app/Http/Controllers/Front/HomepageController.php
@@ -2,13 +2,25 @@
 
 namespace App\Http\Controllers\Front;
 
+use App\Course;
 use App\Http\Controllers\Controller;
+use App\Student;
+use App\Test;
+use App\Trainer;
 use Illuminate\Http\Request;
 
 class HomepageController extends Controller
 {
     //
     public function index(){
-        return view('front.index');
+        $data['courses']=Course::select('id','name','small_desc','cat_id','img','trainer_id')
+        ->orderBy('id','asc')
+        ->take(3)
+        ->get();
+$data['courses_count']=Course::count();
+$data['trainers_count']=Trainer::count();
+$data['students_count']=Student::count();
+$data['tests']=Test::all();
+        return view('front.index')->with($data);
     }
 }
